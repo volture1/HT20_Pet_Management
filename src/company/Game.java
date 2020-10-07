a93abf43da06fc2603df8c8db662b21081599211
Tobias Ostergren <ostergrentobias@gmail.com>
2020-10-07 13:13:07 +0200

    Added feature to create new game.

--- src/company/Game.java
+++ src/company/Game.java
@@ -22,10 +22,35 @@ public class Game {
 
     }
     static void newGame(){
-        boolean correct;
         int rounds, playerAmount;
-
         Scanner input = new Scanner(System.in);
+        //SETTING ROUNDS
+        do{
+            System.out.println("Enter how many rounds you would like to play (5-30)");
+            rounds = input.nextInt();
+            if(rounds < 5){
+                System.out.println("Please enter a number larger than 5.");
+            }
+            else if(rounds > 30){
+                System.out.println("Please enter a number lower than 30.");
+            }
+        }while(rounds < 5 || rounds > 30);
+        //SETTING PLAYERS
+        do{
+            System.out.println("Enter the amount of players for this game (1-4)");
+            playerAmount = input.nextInt();
+
+            if(playerAmount < 1){
+                System.out.println("Please enter a number larger than 1.");
+            }
+            else if(playerAmount > 4){
+                System.out.println("Please enter a number lower than 4.");
+            }
+        }while(playerAmount < 1 || playerAmount > 4);
+
+        System.out.println("----RULES----");
+        System.out.println("Amount of rounds: " + rounds);
+        System.out.println("Amount of players: " + playerAmount);
     }
 
 }
